397ccc67d887209d8d4c8371eb0f2fd926dcbd66
kisdry <kisdry@aliyun.com>
2021-05-21 18:45:23 +0800

    add log

--- cleanflow.c
+++ cleanflow.c
@@ -1,35 +1,45 @@
 #include <argp.h>
 #include <fcntl.h>
-// #include <inttypes.h>
+#include <inttypes.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/prctl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
+#include <rte_branch_prediction.h>
+#include <rte_cycles.h>
+#include <rte_debug.h>
 #include <rte_eal.h>
 #include <rte_launch.h>
-#include <rte_branch_prediction.h>
 #include <rte_log.h>
+#include <rte_timer.h>
 
 #include "cleanflow.h"
+#include "lib/log_ratelimit.h"
+
+uint64_t cycles_per_sec;
+uint64_t cycles_per_ms;
+uint64_t picosec_per_cycle;
+int cleanflow_logtype;
 
 volatile int exiting = false;
 
 static void signal_handler(int signum) {
   if (signum == SIGINT)
-    fprintf(stderr, "caught SIGINT\n");
+    fprintf(stderr, "caught sigint\n");
   else if (signum == SIGTERM)
-    fprintf(stderr, "caught SIGTERM\n");
+    fprintf(stderr, "caught sigterm\n");
   else
     fprintf(stderr, "caught unknown signal (%d)\n", signum);
   exiting = true;
 }
 
-static int run_signal_handler(void) {
+static int signal_process(void) {
   int ret = -1;
   sig_t pipe_handler;
   struct sigaction new_action;
@@ -50,7 +60,7 @@ static int run_signal_handler(void) {
 
   pipe_handler = signal(SIGPIPE, SIG_IGN);
   if (pipe_handler == SIG_ERR) {
-    fprintf(stderr, "Error: failed to ignore SIGPIPE - %s\n", strerror(errno));
+    fprintf(stderr, "error: failed to ignore sigpipe - %s\n", strerror(errno));
     goto term_action;
   }
 
@@ -64,22 +74,74 @@ out:
   return ret;
 }
 
+static int time_resolution_init(void) {
+  int ret;
+  uint64_t diff_ns;
+  uint64_t cycles;
+  uint64_t tsc_start;
+  struct timespec tp_start;
+
+  tsc_start = rte_rdtsc();
+  ret = clock_gettime(CLOCK_MONOTONIC_RAW, &tp_start);
+  if (ret < 0)
+    return ret;
+
+  while (1) {
+    uint64_t tsc_now;
+    struct timespec tp_now;
+
+    ret = clock_gettime(CLOCK_MONOTONIC_RAW, &tp_now);
+    tsc_now = rte_rdtsc();
+    if (ret < 0)
+      return ret;
+
+    diff_ns = (uint64_t)(tp_now.tv_sec - tp_start.tv_sec) * 1000000000ul +
+              (uint64_t)(tp_now.tv_nsec - tp_start.tv_nsec);
+
+    if (diff_ns >= 1000000000ul) {
+      cycles = tsc_now - tsc_start;
+      break;
+    }
+  }
+
+  cycles_per_sec = cycles * 1000000000ul / diff_ns;
+  cycles_per_ms = cycles_per_sec / 1000ul;
+  picosec_per_cycle = 1000ul * diff_ns / cycles;
+
+  G_LOG(NOTICE,
+        "main: cycles/second = %" PRIu64 ", cycles/millisecond = %" PRIu64
+        ", picosec/cycle = %" PRIu64 "\n",
+        cycles_per_sec, cycles_per_ms, picosec_per_cycle);
+
+  return 0;
+}
+
 int main(int argc, char **argv) {
-  int ret = 0;
+
+  int ret;
 
   rte_log_register("cleanflow");
 
+  RTE_VERIFY(prctl(PR_SET_KEEPCAPS, 1, 0, 0, 0) == 0);
+
   ret = rte_eal_init(argc, argv);
   if (ret < 0) {
-    rte_exit(EXIT_FAILURE, "Error: rte_eal_init failed!");
+    rte_exit(EXIT_FAILURE, "error: rte_eal_init failed.");
   }
-
   argc -= ret;
   argv += ret;
 
-  ret = run_signal_handler();
+  rte_timer_subsystem_init();
+
+  ret = signal_process();
   if (ret < 0) {
-    return 0;
+    rte_exit(EXIT_FAILURE, "error: signal_process failed.");
+    return ret;
+  }
+
+  ret = time_resolution_init();
+  if (ret < 0) {
+    return ret;
   }
 
   while (likely(!exiting)) {
